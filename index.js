c45585ffe1190a2932f587cbe9c849fba5a13e0e
louai <louaibaghdadi27@gmail.com>
2023-09-22 11:41:06 +0300

    Create an express simple API

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 const port=5000;
 
+//http://localhost:5000/
+
 app.listen(port, () => {
     console.log("Server ready")
 });
 
 
-app.all('*', (req, res) => {
-    res.send('OK');
-  });
+// app.all('*', (req, res) => {
+//     res.send('OK');
+//   });
+
+/* step 3 */
+
+app.get('/test',(req,res) =>{
+    res.status(200).send('ok')
+})
+
+app.get('/time',(req,res)=>{
+
+    const today = new Date();
+
+    var time = today.getHours() + ":" + today.getMinutes();
+
+    res.status(200).send(time)
+})
 
