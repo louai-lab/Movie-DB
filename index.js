e14048b8297847c868288b1c27158ad61fc422d3
louai <louaibaghdadi27@gmail.com>
2023-09-23 12:53:53 +0300

    Set up the basis for CRUD

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port=5000;
+const port = 5000;
 
 //http://localhost:5000/
 
@@ -15,35 +15,59 @@ app.listen(port, () => {
 
 /* step 3 */
 
-app.get('/test',(req,res) =>{
-    res.json({status:200, message:"ok"})
+app.get('/test', (req, res) => {
+    res.json({ status: 200, message: "ok" })
 })
 
-app.get('/time',(req,res)=>{
+app.get('/time', (req, res) => {
 
     const today = new Date();
 
     var time = today.getHours() + ":" + today.getMinutes();
 
-    res.json({status:200, message:time})
+    res.json({ status: 200, message: time })
 })
 
 /* step 4 */
 
-app.get("/hello/:Id", (req, res)=>{
-    if(req.params.Id != undefined){
-        res.json({status:200,message:req.params.Id})
+app.get("/hello/:Id", (req, res) => {
+    if (req.params.Id != undefined) {
+        res.json({ status: 200, message: req.params.Id })
     }
-    else{
-        res.json({status:200,message:""})
+    else {
+        res.json({ status: 200, message: " " })
     }
 })
 
 app.get("/search", (req, res) => {
     if (typeof req.query.s != "undefined") {
-      res.status(200).send("ok , data : " + req.query.s);
+        res.status(200).send("ok , data : " + req.query.s);
     } else {
-      res.status(500).send("you have to provide a search");
+        res.status(500).send("you have to provide a search");
     }
-  });
+});
 
+/* step 5 */
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 }, 
+    { title: 'Avatar', year: 2009, rating: 7.8 }, 
+    { title: 'Brazil', year: 1985, rating: 8 }, 
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }]
+
+
+app.get("/movies/create", (req, res) => {
+
+});
+
+app.get("/movies/read", (req, res) => {
+    res.json({status:200,data:movies})
+});
+
+app.get("/movies/update", (req, res) => {
+
+});
+
+app.get("/movies/delete", (req, res) => {
+
+});
